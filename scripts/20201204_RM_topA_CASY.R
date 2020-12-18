190529d43dd46f644b57e36f344a58524056daeb
raim <raim@tbi.univie.ac.at>
2020-12-18 19:20:44 +0100

    adding first scripts

---
+++ scripts/20201204_RM_topA_CASY.R
@@ -0,0 +1,140 @@
+options(stringsAsFactors=FALSE)
+
+## moving average
+ma <- function(x,n=10,circular=FALSE)
+    stats::filter(x, rep(1/n, n), sides = 2, circular = circular)
+## diameter -> volume
+d2v <- function(x) (x/2)^3*pi *4/3
+
+
+expid <- "20201204_RM_topA"
+PATH <- "/mnt/synmibi/Studierende/DATA/CASY/"
+PATH <- "/data/synmibi/CASY"
+
+in.path <- file.path(PATH,expid)
+out.path <- file.path("~/work/CoilHack/experiments/reactor/pcc6803/",
+                      expid,"analysis")
+
+## experiment parameters
+dil <- 4000
+
+## analysis&plot parameters
+normalize <- TRUE
+max.cnt <- 10e6 # max cell count for color scheme breaks
+## size filter
+min.counts <- 1.5
+max.counts <- 5
+min.norm <- min.counts
+max.norm <- max.counts
+
+## TODO
+## *calculate median and peak sizes/volumes
+## *calculate total cell volume
+
+files <- list.files(path=in.path, pattern="measurement_.*\\.TXT$")
+
+sizes <- counts <- matrix(NA, nrow=1024, ncol=length(files))
+sampleIDs <- rep(NA, length(files))
+for ( i in seq_along(files) ) {
+    file.name <- files[i]
+    data <- read.delim(file.path(in.path, file.name),header=FALSE)
+    comment <- toupper(data[which(data[,1]=="Comment 1"),2])
+
+    cat(paste("parsing", comment, "\n"))
+    sampleIDs[i] <- comment
+    
+    from <- which(data[,1]=="Size Channel")+1
+    to <- which(data[,1]=="Counts Repeat 1")-1
+    dat <- data[from:to,]
+
+    counts[,i] <- as.numeric(dat[,2])*dil
+    sizes[,i] <- as.numeric(gsub(" ","",dat[,1]))
+    #plot(dat, type="l")
+}
+
+## filter data
+filter <- grep("SAMPLE",sampleIDs)
+filter <- filter[grep("_[A-Z]", sampleIDs[filter], invert=TRUE)]
+counts <- counts[,filter]
+sizes <- sizes[,filter]
+sampleIDs <- sampleIDs[filter]
+
+sampleLabels <- sub(" .*","",sub("SAMPLE_","",sampleIDs))
+
+## check that all sizes are the same
+if ( unique(apply(sizes,1,function(x) length(unique(x))))!=1 )
+    stop("different size vectors!")
+size <- sizes[,1]
+
+## filters
+size.counts <- size>=min.counts & size<=max.counts
+size.norm   <- size>=min.norm & size<=max.norm
+
+## add half of difference to duplicate x (size) values
+dups <- which(duplicated(size))
+size[dups] <- size[dups] + median(diff(size))/2
+
+## total cell number in size range W/O debris
+idx <- size.counts
+
+## total cell number, cells/mL
+total <- apply(counts[idx,],2,sum)
+## total cell volume, uL/mL - TODO: numbers seem too high?
+volume <- apply(counts[idx,],2, function(x) sum(x*d2v(size[idx]))/1e9)
+
+## plot to size range W/O debris
+idx <- size.norm
+
+## colors and breaks
+cols <- viridis::viridis(100)
+#cols <- grey.colors(100,start=1,end=0)
+brks <- seq(0,max.cnt,length.out=101) #1
+
+## normalize counts
+counts.nrm <- counts[idx,] # apply(counts[idx,],2,function(x) x/max(x))
+if ( normalize ) {
+    counts.nrm <-apply(counts[idx,],2,function(x) x/max(x))
+    brks <- brks/max(brks)
+}
+
+## moving average of counts
+counts.nrm <- apply(counts.nrm,2,ma)
+
+
+png(file.path(out.path,paste0(expid,"_CASY.png")),
+    width=2*3.5, height=2*3.5, units="in", res=300)
+par(mfcol=c(2,1),mai=c(.5,1,.1,.5),mgp=c(1.3,.4,0),tcl=-.25,xaxs="i",yaxs="i")
+image(y=d2v(size[idx]),x=1:ncol(counts),z=t(counts.nrm), col=cols,breaks=brks,
+      ylab=expression("cell volume, "*fL), xlab="",ylim=c(0,30),
+      axes=FALSE)
+axis(1, at=1:ncol(counts), label=sampleLabels,las=2)
+axis(2)
+box()
+par(new=TRUE)
+plot(1:ncol(counts), total/1e8, type="p",col=2,
+     axes=FALSE,xlab=NA,ylab=NA,ylim=c(0,11),
+     xlim=par("usr")[1:2],pch=19)
+lines(1:ncol(counts), total/1e8,col=2)
+axis(4,col=2,col.axis=2)
+mtext("1e8 cells/mL",4, par("mgp")[1],col=2)
+par(new=TRUE)
+plot(1:ncol(counts), volume, type="p",col="white",
+     axes=FALSE,xlab=NA,ylab=NA,ylim=c(0,6.5),
+     xlim=par("usr")[1:2],pch=3,lwd=2)
+lines(1:ncol(counts), volume,col="white")
+axis(2,col="black",col.axis="black",line=par("mgp")[1]*2)
+mtext(expression("total cell volume, "*mu*L/mL),2, 3*par("mgp")[1],col="black")
+legend("topleft",c("cell count","total cell volume"),pch=c(19,3),
+       col=c("red","white"),bty="n",text.col="white",pt.lwd=c(1,2))
+#dev.off()
+
+sample.cols <- rev(viridis::viridis(ncol(counts)))
+
+#png(paste0(expid,"_raw.png"), width=400, height=200)
+#par(mai=c(.5,.5,.1,.1),mgp=c(1.3,.4,0),tcl=-.25,xaxs="i",yaxs="i")
+matplot(d2v(size), counts,type="l",lty=1,xlim=c(0,30),
+        col=sample.cols,xlab=expression("cell volume, "*fL),ylim=c(0,1.5e7))
+legend("topright", sampleLabels, col=sample.cols,
+       lty=1,y.intersp=.6,cex=.6,bty="n")
+dev.off()
+
---
+++ scripts/20201204_RM_topA_Specord.R
@@ -0,0 +1,75 @@
+
+options(stringsAsFactors=FALSE)
+
+expid <- "20201204_RM_topA"
+PATH <- "/mnt/synmibi/Studierende/DATA/Specord/"
+PATH <- "/data/synmibi/Specord"
+
+in.path <- file.path(PATH,expid)
+out.path <- file.path("~/work/CoilHack/experiments/reactor/pcc6803/",
+                      expid,"analysis")
+
+
+## switch between normalization wavelengths
+nrm <-443 # 684 # NULL # # reference wavelength for normalization
+REF <- "SAMPLE_07" # as.character(7) ## reference sample
+#REF2 <- as.character(-2) ## reference sample
+
+files <- list.files(path=in.path, pattern="SAMPLE_.*\\.csv$")
+
+smp <- sub("\\.csv","",files)
+wl <- 400:750 # wavelengths
+spc <- matrix(NA, ncol=length(wl), nrow=length(files))
+rownames(spc) <- smp
+
+## load spectra from Specord
+for ( i in seq_along(files) ) {
+    dat <- read.csv(file.path(in.path,files[i]), sep=";",skip=1)
+    dat <- dat[,grep("SAMPLE",colnames(dat))]
+    spc[i,] <- dat 
+}
+
+   
+## sample colors
+cols <- sub("FF$","99",rev(viridis::viridis(nrow(spc))))
+ltys <- rep(c(1,2), length(cols)/2)
+
+nspc <- spc
+    
+## subtract 750 nm baseline
+spc <- spc - spc[,wl==750]
+    
+nspc <- spc
+## normalize to max
+if ( !is.null(nrm) )
+    nspc <- spc/spc[,wl==nrm] #apply(spc,1,max) #max at 443 nm
+
+## calculate ratios
+rspc <- t(t(as.matrix(nspc)) / unlist(nspc[smp==REF,]))
+rspc2 <- t(t(as.matrix(spc))/unlist(spc[smp==REF,]))
+    
+    
+### PLOTS
+    
+##
+png(file.path(out.path,paste0(expid,"_Specord.png")),
+    units="in", height=2*3.5, width=2*3.5, res=200)
+par(mfcol=c(2,2),mai=c(.25,.35,.1,.1), mgp=c(1.1,.1,0), tcl=.25)
+matplot(x=wl,t(spc),type="l",lwd=2,lty=ltys,col=cols,
+        xlab=NA,ylab=expression("corrected absorbance"~A[corr]))
+axis(3, labels=NA, tcl=.25)
+matplot(x=wl,t(nspc),type="l",lwd=2,lty=ltys,col=cols,
+        xlab=NA,ylab=expression("normalized absorbance"~A[norm]))
+legend("bottomleft",smp, col=cols,lty=ltys,lwd=2, cex=.75,
+           y.intersp=.7,bty="n",
+       ncol=ifelse(length(smp)>12,2,1))
+axis(3, labels=NA, tcl=.25)
+mtext("wavelength, nm", 3, 0)
+matplot(x=wl,t(rspc2),type="l",lwd=2,lty=ltys,col=cols,
+        xlab=NA,ylab=bquote(ratio~A[coor]/A[.(REF)]))
+axis(3, labels=NA, tcl=.25)
+matplot(x=wl,t(rspc),type="l",lwd=2,lty=ltys,col=cols,
+        xlab=NA,ylab=bquote(ratio~A[norm]/A[.(REF)]))
+axis(3, labels=NA, tcl=.25)
+mtext("wavelength, nm", 3, 0)
+dev.off()
